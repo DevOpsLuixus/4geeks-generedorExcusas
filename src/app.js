a9101ce7ff9afa32bd22ac4ab28eaef6a7eab5c7
DevOpsLuixus <ing.luismadrid@gmail.com>
2021-12-06 23:21:23 +0000

    sync

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGenerator(who, what, when) {
+  let numeroWho = Math.floor(Math.random() * who.length);
+  let numeroWhat = Math.floor(Math.random() * what.length);
+  let numeroWhen = Math.floor(Math.random() * when.length);
+  return (
+    who[numeroWho] +
+    " " +
+    what[numeroWhat] +
+    " " +
+    "my homework" +
+    " " +
+    when[numeroWhen]
+  );
+}
+function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = excuseGenerator(who, what, when);
+}
+window.onload = onLoad;
